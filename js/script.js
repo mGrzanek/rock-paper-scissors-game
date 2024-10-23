b08506b3c999d38b3e8fe4c1dabf67fd5b4a8090
Monika Grzanek <grzanekmonika@o2.pl>
2024-10-23 12:32:09 +0200

    Add conditional statement with the game result for moves playerMove and computerMove

--- js/script.js
+++ js/script.js
@@ -25,3 +25,25 @@ if(playerInput === '1') {
 printMessage(`Mój ruch to: ${computerMove}`);
 printMessage(`Ruch gracza to: ${playerMove}`);
 
+if (computerMove === 'kamień' && playerMove === "nożyce") {
+    printMessage('Ja wygrywam!')
+} else if (computerMove === 'kamień' && playerMove === "kamień") {
+    printMessage("Remis!");
+} else if (computerMove === "kamień" && playerMove === "papier") {
+    printMessage("Ty wygrywasz!");
+} else if (computerMove === 'papier' && playerMove === "kamień") {
+    printMessage("Ja wygrywam!");
+} else if (computerMove === "papier" && playerMove === "papier") {
+    printMessage("Remis!");
+} else if (computerMove === "papier" && playerMove === "nożyce") {
+    printMessage("Ty wygrywasz!");
+} else if (computerMove === "nożyce" && playerMove === "kamień") {
+    printMessage("Ty wygrywasz!");
+} else if (computerMove === "nożyce" && playerMove === "nożyce") {
+    printMessage("Remis!");
+} else if(computerMove === "nożyce" && playerMove === "papier") {
+    printMessage("Ja wygrywam!");
+} else {
+    printMessage("Upsss... Something went wrong. Try again.");
+}
+
