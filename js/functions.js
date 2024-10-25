7204cf6f1d19517f88c53c68a8eb311d0ef49f05
Monika Grzanek <grzanekmonika@o2.pl>
2024-10-25 11:41:16 +0200

    Move getMoveName and displayResult function declarations from script.js to functions.js

--- js/functions.js
+++ js/functions.js
@@ -22,3 +22,46 @@ function nameCheck(name, newValue) {
         return name;
     }
 }
+
+function getMoveName(argMoveId){
+	if(argMoveId == 1){
+		return 'kamień';
+	} else if (argMoveId == 2) {
+		return 'papier';
+	} else if (argMoveId == 3) {
+		return 'nożyce';
+	}
+	printMessage(`Nie znam ruchu o id ${argMoveId}`);
+	return 'nieznany ruch';
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+	printMessage(`Zagrałem ${computerMove}, a Ty ${playerMove}.`);
+	if (argComputerMove === 'kamień' && argPlayerMove === "nożyce") {
+		printMessage("Tym razem przegrywasz :(")
+		computerResult++;
+	} else if (argComputerMove === 'kamień' && argPlayerMove === "kamień") {
+		printMessage("Remis!");
+	} else if (argComputerMove === "kamień" && argPlayerMove === "papier") {
+		printMessage("Ty wygrywasz!");
+		playerResult++;
+	} else if (argComputerMove === 'papier' && argPlayerMove === "kamień") {
+		printMessage("Tym razem przegrywasz :(");
+		computerResult++;
+	} else if (argComputerMove === "papier" && argPlayerMove === "papier") {
+		printMessage("Remis!");
+	} else if (argComputerMove === "papier" && argPlayerMove === "nożyce") {
+		printMessage("Ty wygrywasz!");
+		playerResult++;
+	} else if (argComputerMove === "nożyce" && argPlayerMove === "kamień") {
+		printMessage("Ty wygrywasz!");
+		playerResult++;
+	} else if (argComputerMove === "nożyce" && argPlayerMove === "nożyce") {
+		printMessage("Remis!");
+	} else if(argComputerMove === "nożyce" && argPlayerMove === "papier") {
+		printMessage("Tym razem przegrywasz :(");
+		computerResult++;
+	} else {
+		printMessage("Upsss... Something went wrong. Try again.");
+	}
+}
